4c21913af176da8b28aba9de625c8759e1eed669
Niklas <niklas-krueger@gmx.de>
2018-12-03 15:43:07 +0100

    Header mit den Farbcodes

---
+++ color-definitions.h
@@ -0,0 +1,28 @@
+#ifndef COLOR_DEFINITIONS_H_INCLUDED
+#define COLOR_DEFINITIONS_H_INCLUDED
+
+#define COLOR_reset "\033[0m"
+
+#define COLOR_bold "\033[1m"
+#define COLOR_underline "\033[4m"
+#define COLOR_swap_FG_BG "\033[7m"
+
+#define COLOR_FG_black "\033[30m"
+#define COLOR_FG_red "\033[31m"
+#define COLOR_FG_green "\033[32m"
+#define COLOR_FG_yellow "\033[33m"
+#define COLOR_FG_blue "\033[94m"
+#define COLOR_FG_magenta "\033[35m"
+#define COLOR_FG_cyan "\033[36m"
+#define COLOR_FG_white "\033[37m"
+
+#define COLOR_BG_black "\033[40m"
+#define COLOR_BG_red "\033[41m"
+#define COLOR_BG_green "\033[42m"
+#define COLOR_BG_yellow "\033[43m"
+#define COLOR_BG_blue "\033[44m"
+#define COLOR_BG_magenta "\033[45m"
+#define COLOR_BG_cyan "\033[46m"
+#define COLOR_BG_white "\033[47m"
+
+#endif // COLOR_DEFINITIONS_H_INCLUDED
